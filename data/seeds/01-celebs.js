bd9ac890cddd1b5c4c5522a3f4b2f589c00fe4fa
chikechris <cchike2017@gmail.com>
2019-09-21 16:02:33 -0400

    added some data to seed

--- data/seeds/01-celebs.js
+++ data/seeds/01-celebs.js
@@ -1,13 +1,18 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = function (knex) {
+  return knex('celebs').insert([
+    {
+      name: 'Tupac Shakur',
+      isDead: true,
+      born: 1971,
+      died: 1996
+    },
+    {
+      name: 'Biggie Smalls',
+      isDead: true,
+      born: 1972,
+      died: 1997
+    }
+  ])
+}
+
