5a9d6f2b67138cd2f16fb94ed6814bb1bd11d2eb
chikechris <cchike2017@gmail.com>
2019-09-23 18:40:47 -0400

    updated migration file

--- data/migrations/20190922081012_create-celebs_and_users_tables.js
+++ data/migrations/20190922081012_create-celebs_and_users_tables.js
@@ -1,32 +1,31 @@
-
 exports.up = function (knex) {
   return knex.schema
     .createTable('celebs', celebs => {
-      celebs.increments();
+      celebs.increments()
       celebs
         .text('name', 128)
         .notNullable()
-        .unique();
-      celebs.text('image_url');
-      celebs.text('info');
-      celebs.date('born').notNullable();
-      celebs.date('died');
-      celebs.boolean('isDead');
+        .unique()
+      celebs.text('image_url')
+      celebs.text('info')
+      celebs.integer('born').notNullable()
+      celebs.integer('died')
+      celebs.boolean('isDead')
     })
 
     .createTable('users', users => {
-      users.increments();
-      users.text('name', 128).notNullable();
+      users.increments()
+      users.text('name', 128).notNullable()
       users
         .text('username', 128)
         .notNullable()
-        .unique();
-      users.text('password', 128).notNullable();
-      users.integer('points', 128);
-    });
-};
+        .unique()
+      users.text('password', 128).notNullable()
+      users.integer('points', 128)
+    })
+}
 
 exports.down = function (knex) {
-  knex.schema.dropTableIfExists('users');
-  return knex.schema.dropTableIfExists('celebs');
-};
+  knex.schema.dropTableIfExists('users')
+  return knex.schema.dropTableIfExists('celebs')
+}
